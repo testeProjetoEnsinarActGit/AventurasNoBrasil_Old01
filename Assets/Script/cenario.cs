907c853071292d5ea46a212a584db715c55060b3
Wesley Xavier de Oliveira <wesleybruce@gmail.com>
2015-09-07 02:46:18 -0300

    versao5

---
+++ Assets/Script/cenario.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cenario : MonoBehaviour {
+
+    // Use this for initialization
+    public Transform action;
+    public float smooth = 0.5f;
+    private float velocidade = 0.5f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float novaPosicao2D = Mathf.SmoothDamp(transform.position.x,action.transform.position.x, ref velocidade, smooth);
+        transform.position = new Vector3(novaPosicao2D, transform.position.y, transform.position.z);
+    }
+}
--- Assets/Script/player.cs
+++ Assets/Script/player.cs
@@ -3,13 +3,35 @@ using System.Collections;
 
 public class player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-}
+    // Use this for initialization
+    
+    public Rigidbody2D rigidbody2D = null;
+    public Transform playerr;
+
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            transform.Translate(Vector2.right * 15 * Time.deltaTime);
+            transform.localEulerAngles = new Vector2(0,0);
+            // cenario.Update(teste);
+        }
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            transform.Translate(Vector2.left * 15 * Time.deltaTime);
+            transform.localEulerAngles = new Vector2(0, 0);
+        }
+    }
+  
+    }
+
+
+
